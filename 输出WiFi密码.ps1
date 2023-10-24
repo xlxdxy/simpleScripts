34ef19a54e8e896c6a12a7c3fa010f09c17e979c
Xusheng <happyhxs@gmail.com>
2023-10-24 13:59:41 +0800

    加了一个WiFi密码是否存在的判断

--- 输出WiFi密码.ps1
+++ 输出WiFi密码.ps1
@@ -1,15 +1,19 @@
-$profiles = (netsh wlan show profiles) | Select-String "所有用户配置文件" 
+$profiles = netsh wlan show profiles | Select-String "所有用户配置文件" 
 foreach ($profile in $profiles) 
 {
     $profileStr = $profile.ToString()
     $index = $profileStr.IndexOf(":")
     $wifiName = $profileStr.Substring($index + 1).Trim()
     
-    $passwordLine = ((netsh wlan show profile name=$wifiName key=clear) | Select-String "关键内容").ToString()
-    $index = $passwordLine.IndexOf(":")
-    $password = $passwordLine.Substring($index + 1).Trim()    #WiFi名称里有冒号就不对了
-
-    Write-Output "Wi-Fi名称: $wifiName,  密码: $password"
+    $wifiInfo = netsh wlan show profile name=$wifiName key=clear
+    if (($wifiInfo | Select-String "安全密钥").ToString().Split(":")[1].Trim() -eq "存在")
+    {
+        $passwordLine = ($wifiInfo | Select-String "关键内容").ToString()
+        $index = $passwordLine.IndexOf(":")
+        $password = $passwordLine.Substring($index + 1).Trim()
+    
+        Write-Output "Wi-Fi名称: $wifiName,  密码: $password"
+    }
 }
 
 
